3e00441c3fea8a014975b9a3cda5b12708108a3c
Trungli <eadiez.u@gmail.com>
2021-10-03 18:16:24 -0400

    First person weights added

--- Assets/FPObjectWeights.cs
+++ Assets/FPObjectWeights.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class FPObjectWeights : MonoBehaviour
 {
     [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    MouseLook playerLookScript;
+
+    [SerializeField]
     GameObject cameraObject;
 
     [SerializeField]
@@ -14,9 +20,16 @@ public class FPObjectWeights : MonoBehaviour
     float targetYRotation;
     float targetXRotationV;
     float targetYRotationV;
+    [SerializeField]
     float rotationSpeed;
+    [SerializeField]
     float holdHeight;
-    float holdSlide;
+    [SerializeField]
+    float holdSide;
+    [SerializeField]
+    float holdDepth;
+    [SerializeField]
+    float moveSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +40,22 @@ public class FPObjectWeights : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        staff1.transform.position = Vector3.Lerp(staff1.transform.position, 
+            cameraObject.transform.position + (Quaternion.Euler(0, targetYRotation, 0) * new Vector3(holdSide, holdHeight, holdDepth)), 
+            Time.deltaTime * moveSpeed);
+
+        staff1.transform.rotation = Quaternion.Euler(Mathf.LerpAngle(staff1.transform.rotation.eulerAngles.x, cameraObject.transform.rotation.eulerAngles.x, Time.deltaTime * rotationSpeed),
+            staff1.transform.rotation.eulerAngles.y,
+            staff1.transform.rotation.eulerAngles.z);
+
+        staff1.transform.rotation = Quaternion.Euler(staff1.transform.rotation.eulerAngles.x,
+            Mathf.LerpAngle(staff1.transform.rotation.eulerAngles.y, cameraObject.transform.rotation.eulerAngles.y, Time.deltaTime * rotationSpeed),
+            staff1.transform.rotation.eulerAngles.z);
+
+    }
+
+    void GetXRotationDueToWeight()
+    {
+
     }
 }
