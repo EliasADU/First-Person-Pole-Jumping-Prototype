1d84d7bb0f2dcc349f84ee3e8858562adca22f15
Trungli <eadiez.u@gmail.com>
2021-10-03 15:35:42 -0400

    setup

---
+++ Assets/FPObjectWeights.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPObjectWeights : MonoBehaviour
+{
+    [SerializeField]
+    GameObject cameraObject;
+
+    [SerializeField]
+    GameObject staff1;
+
+    float targetXRotation;
+    float targetYRotation;
+    float targetXRotationV;
+    float targetYRotationV;
+    float rotationSpeed;
+    float holdHeight;
+    float holdSlide;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
