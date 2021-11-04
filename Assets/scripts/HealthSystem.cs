6a8785dcaf09d574d08c15ac67d72442e4651d2c
santiruz <santiagoruiz1011@gmail.com>
2021-11-04 12:42:48 -0400

    Enemy Increment 2
    Added damage decay formula, made it so enemy goes back to patrolling after losing player, added blood animation to player.

--- Assets/scripts/HealthSystem.cs
+++ Assets/scripts/HealthSystem.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthSystem : MonoBehaviour
 {
-    public float maxHealth = 10000000;
-    public float currentHealth;
+    public float maxHealth;
+    private float currentHealth;
+    public float lowHealth;
+    public Image bloodImage;
 
     public void TakeDamage(float damage)
     {
-        currentHealth -= damage;
-
-        if(currentHealth <= 0)
+        if(damage > 0)
         {
-            Destroy(gameObject);
+            currentHealth -= damage;
+
+            if (currentHealth <= 0)
+            {
+                Destroy(gameObject);
+                ChangeBloodImageAlpha(1f);
+            }
+
+            ChangeBloodImageAlpha(0.8f);
         }
+        
     }
 
     // Start is called before the first frame update
@@ -23,9 +33,36 @@ public class HealthSystem : MonoBehaviour
         currentHealth = maxHealth;
     }
 
+    private void ChangeBloodImageAlpha(float alpha)
+    {
+        if (bloodImage != null)
+        {
+            var color = bloodImage.color;
+            color.a = alpha;
+
+            bloodImage.color = color;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(currentHealth);
+
+        if (bloodImage != null)
+        {
+            var color = bloodImage.color;
+            
+            if(currentHealth <= lowHealth)
+            {
+                color.a = 0.15f;
+            }
+            else
+            {
+                color.a -= 0.01f;
+            }
+
+
+            bloodImage.color = color;
+        }
     }
 }
